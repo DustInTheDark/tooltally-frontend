e16bd529ac6018d40794358031824d60f854ad2d
Sully <tyler12sutherland12@gmail.com>
2025-07-20 16:24:31 +0100

    Removed custom route change. Deleted unused provider implementation to simplify codebase. introduced global loading overlay to display centered spinner.

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+import Spinner from '@/components/Spinner';
+
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/75">
+      <Spinner />
+    </div>
+  );
+}
